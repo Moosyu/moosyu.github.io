469ef4f0e94b7bf2c07fcd070837153cc3468563
Moosy <70246651+Moosyu@users.noreply.github.com>
2023-10-06 19:20:39 +1300

    i wanted to mess with some deleteCells but i have tests
    i cant be wasting my time i need to spend more time studying so its worse when i fail

--- js/tableMaker.js
+++ js/tableMaker.js
@@ -1,25 +1,35 @@
-//some may call the jsonName code bad but i like to call it special
+//some may call this code bad but i like to call it special
 if (document.URL.includes("gamereviews")) {
-    jsonName = "../../../js/media.json"
+    jsonName = "../../../js/games.json"
+	fetch(jsonName).then(function(response){
+		return response.json();
+		}).then(function(results){
+		var output = "";
+		var temp = document.querySelector("#output");
+		for (var result of results){
+		output += `<table id="tableRow1"> 
+			<tr>
+				<td>(${result.name})<br>${result.score}</td>
+				<td>"${result.description}"</td>
+			</tr>
+		</table>`};
+	temp.innerHTML = output});
 }
-else if((document.URL.includes("bookreviews"))) {
+else if(document.URL.includes("bookreviews")) {
 	jsonName = "../../../js/media.json"
+	fetch(jsonName).then(function(response){
+		return response.json();
+		}).then(function(results){
+		var output = "";
+		var temp = document.querySelector("#output");
+		for (var result of results){
+		output += `<table id="tableRow1"> 
+			<tr>
+				<td>[${result.type}]<br>(${result.name})<br>${result.score}</td>
+				<td> <a href="${result.image}" target="_blank"> <img class="tableimg" src="${result.image}"> </a> </td>
+				<td>"${result.description}"</td>
+			</tr>
+		</table>`};
+	temp.innerHTML = output});
 }
 
-
-//document.getElementById("test").textContent=(jsonName);
-fetch(jsonName).then(function(response){
-	return response.json();
-}).then(function(results){
-	var output = "";
-	var temp = document.querySelector("#output");
-	for (var result of results){
-	output += `<table> 
-		<tr>
-			<td>[${result.type}]<br>(${result.name})<br>${result.score}</td>
-			<td> <a href="${result.image}"> <img class="tableimg" src="${result.image}"> </a> </td>
-			<td>"${result.description}"</td>
-		</tr>
-	</table>`};
-temp.innerHTML = output;
-});
