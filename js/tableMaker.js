362ccb64afc1fe5ebce70beafdd069d994c7ed06
Moosy <70246651+Moosyu@users.noreply.github.com>
2024-01-31 15:40:04 +1300

    i got addicted ot hypixel skyblock again whoops
    ill add my favourite albums over time so its not just a big chunk of albums all together

--- js/tableMaker.js
+++ js/tableMaker.js
@@ -1,8 +1,8 @@
 //some may call this code bad but i like to call it special
-if(document.URL.includes("gamereviews")) {
+if(document.URL.includes("game_ratings")) {
 	jsonName = "../../../js/games.json"
 }
-else if (document.URL.includes("abookreviews")) {
+else if (document.URL.includes("abook_ratings")) {
 	jsonName = "../../../js/amedia.json"
 }
 else {
@@ -16,7 +16,7 @@ fetch(jsonName).then(function(response){
 	var output = "";
 	var temp = document.querySelector("#output");
 	for (var result of results){
-	if(document.URL.includes("gamereviews")) {
+	if(document.URL.includes("game_ratings")) {
 		outpuer = `<table id="tableRow1"> 
 		<tr>
 			<td>(${result.name})<br>${result.score}</td>
@@ -24,7 +24,7 @@ fetch(jsonName).then(function(response){
 		</tr>
 		</table>`
 	}
-	else if(document.URL.includes("wbookreviews")) {
+	else if(document.URL.includes("wbook_ratings")) {
 		outpuer = `<table id="tableRow1"> 
 		<tr>
 			<td>[${result.type}]<br>(${result.name})<br>${result.score}</td>
