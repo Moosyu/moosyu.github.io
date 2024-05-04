ab8af0c979cfdddf34c0bebac5e2363ab247eebe
Moosy <70246651+Moosyu@users.noreply.github.com>
2024-05-04 19:37:07 +1200

    im never gonna fix tablemaker i forgot how to js
    sorry for the lack of updates for the past while ive been pretty unmotivated recently even though there are only like 4 people who have ever seen this site but even then its cool getting emails from people who liked it. my lifes kind of been going to shit rn which is kind of a dumb thing for a white male teenager to say as if anything ive won the genetic lucky draw but ive just fumbled the bag so hard. this might sound like a "im in 6th grade every day is a struggle moment"   which it probably is lel but i really am not feeling great right now and somehow my only outlet is an extremely niche shitty geocities inspired site which is kinda silly. anyways have a good day. hopefully ill make this site not shit soon?? no promises though.

--- js/tableMaker.js
+++ js/tableMaker.js
@@ -1,46 +1,44 @@
 //some may call this code bad but i like to call it special
-if(document.URL.includes("game_ratings")) {
+if (document.URL.includes("game_ratings")) {
 	jsonName = "../../../js/games.json"
-}
-else if (document.URL.includes("abook_ratings")) {
+} else if (document.URL.includes("abook_ratings")) {
 	jsonName = "../../../js/amedia.json"
-}
-else {
+} else {
 	jsonName = "../../../js/wmedia.json"
 }
 
 
-fetch(jsonName).then(function(response){
+fetch(jsonName).then(function (response) {
 	return response.json();
-	}).then(function(results){
+}).then(function (results) {
 	var output = "";
 	var temp = document.querySelector("#output");
-	for (var result of results){
-	if(document.URL.includes("game_ratings")) {
-		outpuer = `<table id="tableRow1"> 
+	for (var result of results) {
+		if (document.URL.includes("game_ratings")) {
+			outputer = `<table id="tableRow1"> 
 		<tr>
 			<td>(${result.name})<br>${result.score}</td>
 			<td>"${result.description}"</td>
 		</tr>
 		</table>`
-	}
-	else if(document.URL.includes("wbook_ratings")) {
-		outpuer = `<table id="tableRow1"> 
+		} else if (document.URL.includes("wbook_ratings")) {
+			outputer = `<table id="tableRow1"> 
 		<tr>
 			<td>[${result.type}]<br>(${result.name})<br>${result.score}</td>
 			<td>"${result.description}"</td>
 		</tr>
 		</table>`
-	}
-	else {
-		outpuer =`<table id="tableRow1"> 
+		} else {
+			outputer = `<table id="tableRow1"> 
 			<tr>
 				<td>[${result.type}]<br>(${result.name})<br>${result.score}</td>
-				<td> <a href="${result.image}" target="_blank"> <img loading="lazy" class="tableimg" src="${result.image}"> </a> </td>
+				<td> <a href="${result.image}" target="_blank"> <img loading="lazy" class="tableimg" src="${result.image}"> </a> </td> <!-- the href ="${result.image} redirects to this page if it has no image. idk how to fix this and i dont really want to right now ill do it later i swear. -->
 				<td>"${result.description}"</td>
 			</tr>
 		</table>`
-	}
-	output += outpuer};
-temp.innerHTML = output});
+		}
+		output += outputer
+	};
+	temp.innerHTML = output
+});
 
