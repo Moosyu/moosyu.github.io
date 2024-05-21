3c976378de1a1417e402ff34b9e099f1098afa75
Moosy <70246651+Moosyu@users.noreply.github.com>
2024-05-21 17:40:59 +1200

    oml i forgot to turn on the filter

--- js/comment-widget.js
+++ js/comment-widget.js
@@ -44,7 +44,7 @@ let s_includeUrlParameters = false; // Makes new comment sections on pages with 
 const s_fixRarebitIndexPage = false; // If using Rarebit, change to true to make the index page and page 1 of your webcomic have the same comment section
 
 // Word filter - Censor profanity, etc
-const s_wordFilterOn = false; // True for on, false for off
+const s_wordFilterOn = true; // True for on, false for off
 const s_filterReplacement = '****'; // Change what filtered words are censored with (**** is the default)
 const s_filteredWords = [ // Add words to filter by putting them in quotes and separating with commas (ie. 'heck', 'dang')
     'nigger', 'faggot', 'nigga' //idk any other slurs that are actually bad
