a8f08f0017ea03ee021ab7da51f45d2adb88cfa1
Moosy <70246651+Moosyu@users.noreply.github.com>
2024-05-22 19:43:23 +1200

    a single push and my entire website collapses
    worst code ever made

--- js/rand.js
+++ js/rand.js
@@ -10,8 +10,17 @@
   ]; 
   document.getElementById("china").style.color = colours[Math.floor(Math.random() * colours.length)];
   while (news.length < text.length) {
-    news.push(text[Math.floor(Math.random() * text.length)])
+    let randTemp = ((text[Math.floor(Math.random() * text.length)]))
+    if (! news.includes(randTemp)) { //perhaps the worst code ive ever written. im almost 100% sure there is a better way to do this. this is why when i code in c i always end up with 60 memory leaks.
+      news.push(randTemp) //this code will also blow up your pc if you duplicate something in data.js so watch out
+    }
+  }
+  
+  for (let index = 0; index < news.length; index++) {
+    console.log(news[index])
+    
   }
 
-  document.getElementById('china').innerHTML = news.join('').replace(',', ' - '); //broken rn
+
+  document.getElementById('china').innerHTML = news.join(' - ').replace(',', '');
 }())
