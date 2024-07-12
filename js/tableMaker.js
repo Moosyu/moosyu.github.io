b738801a38d994da8a91f6c54d61268437725047
Moosy <70246651+Moosyu@users.noreply.github.com>
2024-07-12 19:07:18 +1200

    i hate javascript
    pretty sure i didnt slow down the site but im just eyeballing it anyways so i have no real clue.

--- js/tableMaker.js
+++ js/tableMaker.js
@@ -8,30 +8,33 @@ if (document.URL.includes("game_ratings")) {
 }
 
 
+
 fetch(jsonName).then(function (response) {
 	return response.json();
 }).then(function (results) {
+	var scores = [];
 	var output = "";
 	var temp = document.querySelector("#output");
 	for (var result of results) {
+		scores.push(result.score);
 		if (document.URL.includes("game_ratings")) {
 			outputer = `<table> 
 		<tr">
-			<td>(${result.name})<br>${result.score}</td>
+			<td>(${result.name})<br>${result.score}/10</td>
 			<td>"${result.description}"</td>
 		</tr>
 		</table>`
 		} else if (document.URL.includes("wbook_ratings")) {
 			outputer = `<table> 
 		<tr>
-			<td>[${result.type}]<br>(${result.name})<br>${result.score}</td>
+			<td>[${result.type}]<br>(${result.name})<br>${result.score}/10</td>
 			<td>"${result.description}"</td>
 		</tr>
 		</table>`
 		} else {
 			outputer = `<table> 
 			<tr>
-				<td>[${result.type}]<br>(${result.name})<br>${result.score}</td>
+				<td>[${result.type}]<br>(${result.name})<br>${result.score}/10</td>
 				<td> <a href="${result.image}" target="_blank"> <img loading="lazy" class="tableimg" src="${result.image}"> </a> </td> <!-- the href ="${result.image} redirects to this page if it has no image. idk how to fix this and i dont really want to right now ill do it later i swear. -->
 				<td>"${result.description}"</td>
 			</tr>
@@ -40,5 +43,7 @@ fetch(jsonName).then(function (response) {
 		output += outputer
 	};
 	temp.innerHTML = output
+	var result = scores.map(str => parseInt(str, 10)); //the 10 means its doing hexadecimal because what the fuck of course it is
+	document.getElementById("averageScore").innerHTML = "current average score: "+((result.reduce((acc, num) => acc+ num, 0)) / result.length).toFixed(1);
 });
 
