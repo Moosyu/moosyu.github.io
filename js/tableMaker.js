b9ce283dd25f4ce4407fd8f9c2a3e93f8ad7744d
Moosy <70246651+Moosyu@users.noreply.github.com>
2024-08-11 23:24:28 +1200

    im gonna kms
    its so late jesus christ what am i doing

--- js/tableMaker.js
+++ js/tableMaker.js
@@ -7,48 +7,36 @@ function toggleFullscreen(img) {
 	}
 }
 
-if (document.URL.includes("game_ratings")) {
-	jsonName = "https://raw.githubusercontent.com/Moosyu/jsonStorage/main/games.json"
-} else if (document.URL.includes("abook_ratings")) {
-	jsonName = "https://raw.githubusercontent.com/Moosyu/jsonStorage/main/amedia.json"
-} else {
-	jsonName = "https://raw.githubusercontent.com/Moosyu/jsonStorage/main/wmedia.json"
-}
+let ascOrDesc = 'desc';
+var count = 0;
+function buildTable(results) {
+	ascOrDesc = ascOrDesc === 'asc' ? 'desc' : 'asc'; // this is like in the format condition ? expressionIfTrue : expressionIfFalse;
+	console.log(count)
 
-fetch(jsonName).then(function (response) {
-	return response.json();
-}).then(function (results) {
-	var scores = [];
-	var output = "";
-	var temp = document.querySelector("#output");
-	for (var result of results) {
-		scores.push(result.score);
-		if (document.URL.includes("game_ratings")) {
-			outputer = `<table> 
-		<tr">
-			<td>(${result.name})<br>${result.score}/10</td>
-			<td>"${result.description}"</td>
-		</tr>
-		</table>`
-		} else if (document.URL.includes("wbook_ratings")) {
-			outputer = `<table> 
-		<tr>
-			<td>[${result.type}]<br>(${result.name})<br>${result.score}/10</td>
-			<td>"${result.description}"</td>
-		</tr>
-		</table>`
-		} else {
-			outputer = `<table> 
+	count += 1;
+	if (count > 1) results.sort((a, b) => ascOrDesc === 'asc' ? a.score - b.score : b.score - a.score);
+
+	const output = results.map(result => `
+		<table>
 			<tr>
-				<td>[${result.type}]<br>(${result.name})<br>${result.score}/10</td>
-				<td><img loading="lazy" class="tableimg" src="${result.image}" onclick="toggleFullscreen(this)"></td>
+				<td>${jsonName != "https://raw.githubusercontent.com/Moosyu/jsonStorage/main/games.json" ? `[${result.type}]` : ''}<br>(${result.name})<br>${result.score}/100</td>
+				${jsonName == "https://raw.githubusercontent.com/Moosyu/jsonStorage/main/amedia.json" ? `<td><img loading="lazy" class="tableimg" src="${result.image}" onclick="toggleFullscreen(this)"></td>` : ''}
 				<td>"${result.description}"</td>
 			</tr>
-		</table>`
-		}
-		output += outputer
-	}
-	temp.innerHTML = output
-	var intStr = scores.map(str => parseInt(str, 10)); //the 10 means its doing hexadecimal because what the fuck of course it is
-	document.getElementById("averageScore").innerHTML = "current average score: "+((intStr.reduce((acc, num) => acc+ num, 0)) / intStr.length).toFixed(1);
+		</table>
+	`).join('');
+
+	document.querySelector("#output").innerHTML = output;
+}
+
+const jsonName = document.URL.includes("game_ratings") ? "https://raw.githubusercontent.com/Moosyu/jsonStorage/main/games.json" : document.URL.includes("abook_ratings") ? "https://raw.githubusercontent.com/Moosyu/jsonStorage/main/amedia.json" : "https://raw.githubusercontent.com/Moosyu/jsonStorage/main/wmedia.json";
+
+fetch(jsonName).then(response => response.json())
+	.then(results => {
+		buildTable(results);
+
+		document.getElementById("sortScore").addEventListener("click", () => buildTable(results));
+		const averageScore = (results.reduce((acc, { score }) => acc + parseInt(score, 10), 0) / results.length);
+		document.getElementById("averageScore").innerHTML = "current average score: " + averageScore.toFixed(0);
+		document.getElementById("averageScore").title = averageScore.toFixed(2);
 });
