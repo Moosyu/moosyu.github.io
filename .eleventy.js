194c7b95d69b6dcbe0a49e42f91951eea1396179
Moosy <70246651+Moosyu@users.noreply.github.com>
2024-09-13 08:18:04 +1200

    third timeas the charm
    i think it will hopefully work now. if it doesnt ill revert it and fix it later today

--- .eleventy.js
+++ .eleventy.js
@@ -30,6 +30,7 @@ module.exports = function(eleventyConfig) {
       },
       markdownTemplateEngine: "njk",
       htmlTemplateEngine: "njk",
-      dataTemplateEngine: "njk"
+      dataTemplateEngine: "njk",
+      pathPrefix: "/"
     };
   };
