2126adc8f295fbcf826becb67076414835b8106e
Moosy <70246651+Moosyu@users.noreply.github.com>
2024-09-16 17:21:28 +1200

    added inspirations back
    might move it later tho but i got stuff to do rn

---
+++ src/js/collapse.js
@@ -0,0 +1,16 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+function collapse() {
+    for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+}
