aa3e9fdc3468ab9aec73d68f85b88bd345157f00
Moosy <70246651+Moosyu@users.noreply.github.com>
2025-04-13 14:32:17 +1200

    navbar looks better now (i think)
    also lastfm and github fetching use the same style

--- src/js/lastFM.js
+++ src/js/lastFM.js
@@ -1,12 +1,15 @@
-const url = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=moosyu&api_key=e3b58abffa64551cc31a214fdf5e1870&format=json&limit=1`;
+(async () => {
+    try {
+        const response = await fetch('https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=moosyu&api_key=e3b58abffa64551cc31a214fdf5e1870&format=json&limit=1');
+        const data = await response.json();
+        const track = data.recenttracks.track[0];
 
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-    const track = data.recenttracks.track[0];
-
-    document.getElementById("listening").innerHTML = `
-    <div id="trackInfo">
-    <span>Last heard: <a href="${track.url}">${track.name}</a> by ${track.artist['#text']}</span>
-    </div>`
-}).catch(error => console.error('fetching last.fm failed:', error));
+        document.getElementById("listening").innerHTML = `
+        <div id="trackInfo">
+            <span>Last heard: <a href="${track.url}">${track.name}</a> by ${track.artist['#text']}</span>
+        </div>`;
+    
+    } catch (error) {
+        console.error("Fetching LastFM failed:", error);
+    }
+})();
